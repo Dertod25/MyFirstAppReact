11e70e4854b5a75ac3e70d271b7368cf5dd2d1d7
yaroslav <yaroslav@kingmuffin.com>
2017-11-15 21:15:02 +0200

    fix : MEIS-249  Store in the database the email address of the athletes in lowercase

--- generateChangelog.js
+++ generateChangelog.js
@@ -1,7 +1,7 @@
 var Changelog = require('generate-changelog');
 var Fs        = require('fs');
 
-return Changelog.generate({  repoUrl: 'https://github.com/lob/generate-changelog' })
+return Changelog.generate({  repoUrl: 'https://github.com/Dertod25/MyFirstAppReact' })
     .then(function (changelog) {
-        Fs.writeFileSync('./CHANGELOG.md', changelog);
+        Fs.writeFileSync('./HYSTORY.md', changelog);
     });
